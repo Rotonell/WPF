d7dc24598718e2f47fb505db477ecc75267133d6
Дарья Серова <dashaflay2000@gmail.com>
2026-04-26 16:44:15 +0300

    Reusable MenuBar DONE

---
+++ MenuBar/View/UsersControls/MenuBar.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MenuBar.View.UserControls
+{
+	public partial class MenuBar : UserControl
+	{
+		public MenuBar()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
